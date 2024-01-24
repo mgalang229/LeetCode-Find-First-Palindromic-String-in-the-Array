3109326cc66d6cdecb78781b9814aef29299be68
jasonpogi <51401355+mgalang229@users.noreply.github.com>
2024-01-24 21:01:02 +0800

    Update Solution.cpp

--- Solution.cpp
+++ Solution.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    string firstPalindrome(vector<string>& words) {
-        for (auto& w : words) {
-            int n = w.size();
-            bool p = true;
-            for (int i = 0; i < n / 2; i++) {
-                p &= (w[i] == w[n - 1 - i]);
-            }
-            if (!p) {
-                continue;
-            }
-            return w;
-        }
-        return "";
+  string firstPalindrome(vector<string>& words) {
+    for (auto& w : words) {
+      int n = w.size();
+      bool p = true;
+      for (int i = 0; i < n / 2; i++) {
+        p &= (w[i] == w[n - 1 - i]);
+      }
+      if (!p) {
+        continue;
+      }
+      return w;
     }
+    return "";
+  }
 };
